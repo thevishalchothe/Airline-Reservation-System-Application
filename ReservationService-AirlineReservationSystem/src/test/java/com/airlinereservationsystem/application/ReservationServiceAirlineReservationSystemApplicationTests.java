95f4b23c0ee3433d0f409288c122cbe0afc4e9fd
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-01-11 23:09:16 +0530

    Create Reservation Service for AirlineReservationSystem Application

---
+++ ReservationService-AirlineReservationSystem/src/test/java/com/airlinereservationsystem/application/ReservationServiceAirlineReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airlinereservationsystem.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationServiceAirlineReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
