cd465a7a10c5f3040976217614289441b43c38fb
Vishal Chothe <163386224+thevishalchothe@users.noreply.github.com>
2025-01-11 22:18:25 +0530

    Add Flight entity to Flight Service ARS project

--- FlightService-AirlineReservationSystem/src/main/java/com/airlinereservationsystem/application/model/Flight.java
+++ FlightService-AirlineReservationSystem/src/main/java/com/airlinereservationsystem/application/model/Flight.java
@@ -1,5 +1,37 @@
 package com.airlinereservationsystem.application.model;
 
-public class Flight {
+import java.util.Date;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Flight {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long flightId;
+
+	private String flightName;
+	private String source;
+	private String destination;
+	private int totalSeats;
+	private int availableSeats;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date departureTime;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date arrivalTime;
+	
 }
